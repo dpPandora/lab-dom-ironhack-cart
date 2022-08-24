dba7967874c735310642641ed07d5e6e2d8d80a5
Pandora <hadrian.diaz@gmail.com>
2022-08-24 12:14:13 -0400

    completed required iterations

--- js/index.js
+++ js/index.js
@@ -2,6 +2,14 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span');
+  const quant = product.querySelector('.quantity input');
+  //alert(price.innerText + quant.value);
+  let subtotal = price.innerText * quant.value;
+  //alert(subtotal);
+  product.querySelector('.subtotal span').innerText = subtotal;
+
+  return subtotal;
 
   //... your code goes here
 }
@@ -9,12 +17,20 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  const products = document.querySelectorAll(".product");
+  let total = 0;
+  //alert(products.length);
+  for (let index = 0; index < products.length; index++) {
+    total += updateSubtotal(products[index]);
+  }
+  //alert(total);
+  document.querySelector('#total-value span').innerText = total;
 
   // ITERATION 3
   //... your code goes here
@@ -39,4 +55,5 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
+  
 });
